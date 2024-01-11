b51ef207af403945b2c82c51fad3bfce5f1671b5
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-10 23:23:38 -0500

    prep

--- lib/book.py
+++ lib/book.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-
+import ipdb
 class Book:
     def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
-
-
+    
     def get_page_count(self):
-        return self._page_count   
+        return self._page_count
     
     def set_page_count(self, page_count):
         if isinstance(page_count, int):
             self._page_count = page_count
-        else:
-            print('page_count must be an integer')
+        print('page_count must be an integer')
 
+    page_count = property(get_page_count, set_page_count)
     def turn_page(self):
         print('Flipping the page...wow, you read fast!')
-    page_count = property(get_page_count, set_page_count)
+        
+# ipdb.set_trace()   
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
-
+import ipdb
 class Shoe:
     def __init__(self, brand, size):
         self.brand = brand
         self.size = size
-
+    # ipdb.set_trace()
     def get_size(self):
         return self._size
+
     def set_size(self, size):
         if isinstance(size, int):
             self._size = size
-        else:
-            print('size must be an integer')
-    size = property(get_size, set_size)
+        print('size must be an integer')
+    size=property(get_size, set_size)
 
     def cobble(self):
-        self.condition = 'New'
         print('Your shoe is as good as new!')
+        self.condition = 'New'
 
-adidas = Shoe('nike', 10)
-adidas.set_size(11)
-print(adidas.cobble())
-print(adidas.condition)
+
+
+    
